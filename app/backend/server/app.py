9bbbc65439594b901e9e7ca06429ff9bb2cccdaf
AppleRadium <fleming2001@protonmail.com>
2024-03-23 18:07:43 -0500

    middleware

--- app/backend/server/app.py
+++ app/backend/server/app.py
@@ -1,23 +1,23 @@
 from fastapi import FastAPI, HTTPException, WebSocket
 from server.routes.fooditem import router as FoodRouter
-#from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 #from typing import List
 #from uuid import UUID, uuid4
 
 app = FastAPI()
 app.include_router(FoodRouter, tags=["Food Item"], prefix="/fooditem")
-"""origins = [
-    "http://localhost:3000",
-]"""
+origins = [
+    "*"
+]
 
 
-"""app.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)"""
+)
 
 
 @app.get("/")
--- app/frontend/test.py
+++ app/frontend/test.py
@@ -4,4 +4,4 @@ payload = {
   "name": "pizza"
 }
 headers = {"Content-type": "application/json", "Accept": "application/json"}
-r = requests.post("http://localhost:8000/fooditem/", headers=headers, data=payload)
+r = requests.post("http://45.32.223.3:8000/fooditem/", headers=headers, data=payload)
