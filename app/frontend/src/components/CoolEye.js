071f0a3b64411e08275f77366850ac64169dba03
appleradium@gmail.com <fleming2001@protonmail.com>
2024-04-02 14:53:03 -0500

    frontend5

--- app/frontend/src/components/CoolEye.js
+++ app/frontend/src/components/CoolEye.js
@@ -2,20 +2,23 @@ import axios from 'axios'
 import React from 'react'
 
 function Inventory(props) {
-    const deleteFoodItem = (Item) => {
-        axios.delete(`https://protected-dawn-61147-56a85301481c.herokuapp.com/fooditem/${Item}`)
-            .then(res => console.log(res.data)) 
-            .then(data => console.log(data))
+    const deleteFoodItem = (itemId) => {
+        // Notice that now we use `itemId` to construct the URL
+        axios.delete(`https://protected-dawn-61147-56a85301481c.herokuapp.com/fooditem/${itemId}`)
+            .then(res => console.log(res.data))
+            .catch(error => console.error('Delete operation failed:', error));
     };
-  
+
     return (
         <div>
             <div>
                 <span style={{ fontWeight: 'bold, underline' }}>{props.fooditem.Item} : </span> {props.fooditem.Count}
-                <button onClick={() => deleteFoodItem(props.fooditem.Item)} className="btn btn-outline-danger my-2 mx-2" style={{'borderRadius':'50px',}}>X</button>
+                {/* Pass the `id` of the item to the deleteFoodItem function when the button is clicked */}
+                <button onClick={() => deleteFoodItem(props.fooditem.id)} className="btn btn-outline-danger my-2 mx-2" style={{'borderRadius':'50px'}}>X</button>
             </div>
             <hr />
         </div>
     );
 }
+
 export default Inventory;
